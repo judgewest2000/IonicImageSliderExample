8d9032cc91edfc1393dfa318bff82dee7c9f8918
James West <james.west@westdarley.co.uk>
2018-12-02 23:31:10 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var juxtapose : any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('juxtaposeslider') juxtaposeSlider: ElementRef;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewDidLoad(){
+
+    const getRandomInt = (max:number) => {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    const element = this.juxtaposeSlider.nativeElement as HTMLDivElement;
+    
+    const id = `juxtapose-${getRandomInt(1000000)}`;
+
+    element.id= id;
+
+    const slider = new juxtapose.JXSlider(`#${id}`,
+    [
+        {
+            src: 'assets/images/first.jpg',
+            label: '2009',
+            credit: 'First'
+        },
+        {
+            src: 'assets/images/second.jpg',
+            label: '2014',
+            credit: 'Second'
+        }
+    ],
+    {
+        animate: true,
+        showLabels: true,
+        showCredits: true,
+        startingPosition: "50%"
+    });
+  }
+
+
 
 }
